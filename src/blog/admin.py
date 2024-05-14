61e5b09ef188b68b4be85a11ef83b9ec9d661d9c
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-14 15:06:48 +0100

    feat: Add PostAdmin class for managing blog posts in the admin interface

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "published_date")
+    list_filter = ("published_date", "author")
+    search_fields = ("title", "body")
+    readonly_fields = ("published_date",)
