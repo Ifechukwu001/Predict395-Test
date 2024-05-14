6b17c20df31de693a6dd1be28b310cdd6045d2a9
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-14 16:04:34 +0100

    refactor: Update SignupForm to include confirmation password field

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -7,10 +7,21 @@ from django import forms
 
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(attrs={"autofocus": True}),
+    )
+
     password1 = forms.CharField(
         label="Password",
         strip=False,
         widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+    )
+
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        strip=False,
     )
 
     def _post_clean(self):
