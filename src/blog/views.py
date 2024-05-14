eeef687f9a72d7103f088eba8b0f209c303826ef
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-14 14:51:01 +0100

    feat: Add post_update view for updating blog posts

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Post
@@ -23,3 +23,13 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, "blog/post_detail.html", {"post": post})
+
+
+def post_update(request, pk):
+    post = Post.objects.get(pk=pk)
+    form = PostForm(request.POST or None, instance=post)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect("blog:detail", pk=post.pk)
+    return render(request, "blog/post_update.html", {"form": form})
