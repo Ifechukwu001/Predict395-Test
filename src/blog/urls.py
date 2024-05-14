43faac29d53a82980eec38efa0566cf6752fd698
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-14 14:41:26 +0100

    feat: Add URL patterns for blog post list and detail views

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import post_create
+from .views import post_create, post_list, post_detail
 
 app_name = "blog"
 
 urlpatterns = [
     path("create/", post_create, name="create"),
+    path("", post_list, name="list"),
+    path("<int:pk>/", post_detail, name="detail"),
 ]
