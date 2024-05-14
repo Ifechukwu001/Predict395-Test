24de4c7e08f88f112bdfa8cc339a73e84e80198e
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-14 14:23:25 +0100

    chore: Add login_required decorator to post_create view

---
+++ src/blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from .models import Post
+from .forms import PostForm
+
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.instance.author = request.user
+            form.save()
+    return render(request, "blog/post_create.html", {"form": form})
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, "blog/post_list.html", {"posts": posts})
