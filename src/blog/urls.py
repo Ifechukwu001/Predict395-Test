dc8eba5b057ef2ecf15d38df29c484568385831a
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-14 14:51:07 +0100

    feat: Add post_update URL pattern and view for updating blog posts

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import post_create, post_list, post_detail
+from .views import post_create, post_list, post_detail, post_update
 
 app_name = "blog"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("create/", post_create, name="create"),
     path("", post_list, name="list"),
     path("<int:pk>/", post_detail, name="detail"),
+    path("<int:pk>/update/", post_update, name="update"),
 ]
