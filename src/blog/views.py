3b4fc2848b23b6e1c523440e221223de31824fb9
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-14 14:41:34 +0100

    Add post_detail view to display individual blog posts

--- src/blog/views.py
+++ src/blog/views.py
@@ -18,3 +18,8 @@ def post_create(request):
 def post_list(request):
     posts = Post.objects.all()
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, "blog/post_detail.html", {"post": post})
