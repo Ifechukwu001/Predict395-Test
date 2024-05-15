8d57a2c033d3144b2aae0af456961c8438959f23
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-15 14:15:43 +0100

    refactor: Add redirect from root URL to blog page

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path("", include("accounts.urls")),
+    path("", RedirectView.as_view(url="blog/")),
     path("blog/", include("blog.urls")),
     path("admin/", admin.site.urls),
 ]
